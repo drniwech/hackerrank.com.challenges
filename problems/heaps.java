5be34980a2e6de467efad49b28b4b196502d03a1
Dr. Niwech Harnkham <35854141+drniwech@users.noreply.github.com>
2018-11-03 17:29:57 -0500

    Create heaps.java

---
+++ problems/heaps.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+/*
+  Heaps: Find the Running Median
+*/
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        NavigableMap<String, Object> contacts = new TreeMap<String, Object>();
+        for(int a0 = 0; a0 < n; a0++){
+            String op = in.next();
+            String contact = in.next();
+            if("add".equals(op)){
+                contacts.put(contact, contact);
+            }else if("find".equals(op)){
+               System.out.println(contacts.subMap(contact, contact+Character.MAX_VALUE).size());
+            }
+        }
+    }
+}
